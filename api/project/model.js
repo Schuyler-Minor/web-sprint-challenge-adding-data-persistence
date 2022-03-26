3fcbc764ee43cf2722f958173b2d18dc63e8ce30
Schuyler <skym1n0r91@gmail.com>
2022-03-25 23:16:19 -0500

    project

--- api/project/model.js
+++ api/project/model.js
@@ -10,4 +10,22 @@ async function getAll() {
 
   return projects;
 }
-module.exports = { getAll };
+
+async function getById(id) {
+  const [project] = await db("projects").where("project_id", id);
+
+  if (project) {
+    project.project_completed = !!project.project_completed;
+  }
+
+  return project;
+}
+
+async function create(project) {
+  const [id] = await db("projects").insert(project);
+
+  const created = await getById(id);
+  return created;
+}
+
+module.exports = { getAll, create };
--- api/project/router.js
+++ api/project/router.js
@@ -1,6 +1,7 @@
 // build your `/api/projects` router here
 const router = require("express").Router();
 const Project = require("./model");
+const { validateProject } = require("./middleware");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -11,16 +12,13 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.use("*", (req, res) => {
-  res.json({ api: "project up" });
-});
-
-router.use((err, req, res, next) => {
-  res.status(500).json({
-    customMessage: "something went wrong inside the project router",
-    message: err.message,
-    stack: err.stack,
-  });
+router.post("/", validateProject, async (req, res, next) => {
+  try {
+    const newProject = await Project.create(req.body);
+    res.status(201).json(newProject);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
