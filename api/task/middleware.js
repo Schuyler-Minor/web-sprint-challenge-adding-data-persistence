c3ea4d7076a3a0c179709c3abc2b4dd5b34a44c9
Schuyler <skym1n0r91@gmail.com>
2022-03-25 23:35:15 -0500

    api is connected

---
+++ api/task/middleware.js
@@ -0,0 +1,13 @@
+const Project = require("./../project/model");
+
+function validateTaskBody(req, res, next) {
+  if (!req.body.task_description || !req.body.task_description.trim()) {
+    next({ status: 400, message: "task description is required" });
+  } else if (!req.body.project_id) {
+    next({ status: 400, message: "project id is required" });
+  } else {
+    next();
+  }
+}
+
+module.exports = { validateTaskBody };
--- api/task/router.js
+++ api/task/router.js
@@ -1,16 +1,23 @@
 // build your `/api/tasks` router here
 const router = require("express").Router();
+const Task = require("./model");
+const { checkProjectIdExists, validateTaskBody } = require("./middleware");
 
-router.use("*", (req, res) => {
-  res.json({ api: "task router up" });
+router.get("/", async (req, res, next) => {
+  try {
+    const tasks = await Task.getAll();
+    res.json(tasks);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.use((err, req, res, next) => {
-  res.status(500).json({
-    customMessage: "something went wrong inside the task router",
-    message: err.message,
-    stack: err.stack,
-  });
+router.post("/", validateTaskBody, async (req, res, next) => {
+  try {
+    const newTask = await Task.create(req.body);
+    res.status(201).json(newTask);
+  } catch (err) {
+    next(err);
+  }
 });
-
 module.exports = router;
