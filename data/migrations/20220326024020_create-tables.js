2215635f7db030456f517cc2b978ff4f6d92a908
Schuyler <skym1n0r91@gmail.com>
2022-03-25 22:16:38 -0500

    projects and resources table

---
+++ data/migrations/20220326024020_create-tables.js
@@ -0,0 +1,21 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("projects", (tbl) => {
+      tbl.increments("project_id");
+      tbl.string("project_name", 200).notNullable();
+      tbl.string("project_description", 300);
+      tbl.boolean("project_completed").defaultTo(false);
+    })
+    .createTable("resources", (tbl) => {
+      tbl.increments("resource_id");
+      tbl.string("resource_name", 200).notNullable().unique();
+      tbl.string("resource_description", 200);
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("resources")
+
+    .dropTableIfExists("projects");
+};
