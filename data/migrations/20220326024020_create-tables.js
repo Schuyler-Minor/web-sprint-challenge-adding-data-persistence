978c3ac41619bdfb17951c9845a94462ec4914ae
Schuyler <skym1n0r91@gmail.com>
2022-03-25 22:21:15 -0500

    tables created

--- data/migrations/20220326024020_create-tables.js
+++ data/migrations/20220326024020_create-tables.js
@@ -10,11 +10,46 @@ exports.up = function (knex) {
       tbl.increments("resource_id");
       tbl.string("resource_name", 200).notNullable().unique();
       tbl.string("resource_description", 200);
+    })
+    .createTable("tasks", (tbl) => {
+      tbl.increments("task_id");
+      tbl.string("task_description", 200).notNullable();
+      tbl.string("task_notes", 300);
+      tbl.boolean("task_completed").defaultTo(false);
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("RESTRICT");
+    })
+    .createTable("project_resources", (tbl) => {
+      tbl.increments("project_resource_id");
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("RESTRICT");
+      tbl
+        .integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("resource_id")
+        .inTable("resources")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
     });
 };
 
 exports.down = function (knex) {
   return knex.schema
+    .dropTableIfExists("project_resources")
+    .dropTableIfExists("tasks")
     .dropTableIfExists("resources")
     .dropTableIfExists("projects");
 };
